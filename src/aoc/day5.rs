ef59f885b6ccaf3c18a633169f636ab4f80811f1
Peter Heiss <peter.heiss@uni-muenster.de>
2021-12-05 15:37:46 +0000

    isnert test

--- src/aoc/day5.rs
+++ src/aoc/day5.rs
@@ -3,8 +3,7 @@ use regex::Regex;
 
 const SIZE: usize = 1000;
 
-pub fn part1() -> String {
-    let contents = read_in("inputs/day5.txt");
+fn day5(contents: &str) -> String {
     let re = Regex::new(r"(?P<x1>.*),(?P<y1>.*) -> (?P<x2>.*),(?P<y2>.*)").unwrap();
     let mut arr = [[0u8; SIZE]; SIZE];
 
@@ -36,6 +35,17 @@ pub fn part1() -> String {
     arr.iter().flatten().filter(|&&v| v > 1).count().to_string()
 }
 
+pub fn part1() -> String {
+    let contents = read_in("inputs/day5.txt");
+    day5(&contents)
+}
+
 pub fn part2() -> String {
     String::new()
+}
+
+#[test]
+fn test_input() {
+    let input = "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2";
+    assert_eq!(day5(input), "5");
 }
