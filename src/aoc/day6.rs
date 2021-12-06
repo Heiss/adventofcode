add5cc89735463068941def9431e111574c1d285
Peter Heiss <peter.heiss@uni-muenster.de>
2021-12-06 06:42:40 +0000

    add day6 skeleton

---
+++ src/aoc/day6.rs
@@ -0,0 +1,15 @@
+fn day6(contents: &str) -> String {
+    let needs_days = 7u8;
+    let first_cycle = needs_days + 2u8;
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
