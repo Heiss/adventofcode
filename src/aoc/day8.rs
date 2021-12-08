6e3032350e2e03a02b37a1b9720773794162da1c
Peter Heiss <peter.heiss@uni-muenster.de>
2021-12-08 20:58:13 +0000

    try to impl day8

--- src/aoc/day8.rs
+++ src/aoc/day8.rs
@@ -1,5 +1,6 @@
-use itertools::Itertools;
 use std::collections::HashMap;
+
+use itertools::Itertools;
 
 use crate::helper::read_in;
 
@@ -23,14 +24,10 @@ fn day8_part1(contents: &str) -> u32 {
 }
 
 fn day8_part2(contents: &str) -> u32 {
-    let numbers = [
-        "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg",
-    ];
-
     contents
         .split("\n")
         .map(|line| {
-            let mut map: HashMap<char, char> = HashMap::new();
+            let mut map: HashMap<usize, String> = HashMap::new();
 
             let mut split = line.split("|");
             let left = split.next().expect("no left");
@@ -38,12 +35,12 @@ fn day8_part2(contents: &str) -> u32 {
 
             // find easy digits
             left.split_whitespace()
-                .filter(|&v| match v.len() {
+                .filter(|&word| match word.len() {
                     2 | 3 | 4 | 7 => true,
                     _ => false,
                 })
-                .for_each(|v| {
-                    let number = match v.len() {
+                .for_each(|word| {
+                    let number = match word.len() {
                         2 => 1,
                         3 => 7,
                         4 => 4,
@@ -51,40 +48,100 @@ fn day8_part2(contents: &str) -> u32 {
                         _ => panic!("not an easy digit"),
                     };
 
-                    v.chars().sorted().enumerate().for_each(|(index, c)| {
-                        let m = numbers[number].chars().nth(index).expect("char not found");
-                        if let None = map.get(&m) {
-                            map.insert(m, c);
-                        }
-                    })
+                    map.insert(number, word.to_string());
                 });
 
-            let encode_numbers = numbers
-                .iter()
-                .map(|&v| {
-                    v.chars()
-                        .map(|c| map.get(&c).expect("char not in map"))
-                        .sorted()
-                        .collect::<String>()
-                })
-                .collect::<Vec<String>>();
-
-            right
+            // find 6
+            if let Some(six) = left
                 .split_whitespace()
-                .map(|v| {
-                    let v: String = v.chars().sorted().collect();
-                    encode_numbers
+                .filter(|&word| {
+                    word.len() == 6
+                        && word
+                            .chars()
+                            .any(|c| map.get(&1).expect("no 1 found").chars().all(|f| f != c))
+                })
+                .next()
+            {
+                map.insert(6, six.to_string());
+            }
+
+            // find 0
+            if let Some(zero) = left
+                .split_whitespace()
+                .filter(|&word| {
+                    word.len() == 6
+                        && word
+                            .chars()
+                            .any(|c| !map.get(&4).expect("no 4 found").chars().all(|f| f != c))
+                        && !map.values().any(|f| f == word)
+                })
+                .next()
+            {
+                map.insert(0, zero.to_string());
+            }
+
+            // find 9
+            if let Some(nine) = left
+                .split_whitespace()
+                .filter(|&word| word.len() == 6 && !map.values().any(|f| f == word))
+                .next()
+            {
+                map.insert(9, nine.to_string());
+            }
+
+            // find 5
+            if let Some(five) = left
+                .split_whitespace()
+                .filter(|&word| {
+                    word.len() == 5
+                        && word
+                            .chars()
+                            .all(|c| map.get(&6).expect("no 6 found").chars().any(|f| f == c))
+                })
+                .next()
+            {
+                map.insert(5, five.to_string());
+            }
+
+            // find 3
+            if let Some(three) = left
+                .split_whitespace()
+                .filter(|&word| {
+                    word.len() == 5
+                        && word
+                            .chars()
+                            .all(|c| map.get(&9).expect("no 9 found").chars().any(|f| f == c))
+                        && !map.values().any(|f| f == word)
+                })
+                .next()
+            {
+                map.insert(3, three.to_string());
+            }
+
+            // find 2
+            if let Some(two) = left
+                .split_whitespace()
+                .filter(|&word| word.len() == 5 && !map.values().any(|f| f == word))
+                .next()
+            {
+                map.insert(2, two.to_string());
+            }
+
+            let s = right
+                .split_whitespace()
+                .map(|word| {
+                    let (&num, _) = map
                         .iter()
-                        .position(|n| *n == v)
-                        .expect(&format!(
-                            "encode not found, encode: {:?}, map: {:?}, value: {}",
-                            encode_numbers, map, v
-                        ))
-                        .to_string()
+                        .filter(|(_, val)| word.chars().all(|f| val.chars().any(|c| c == f)))
+                        .next()
+                        .expect(&format!("word not found: {}", word));
+                    num.to_string()
                 })
                 .collect::<String>()
                 .parse::<u32>()
-                .expect("cannot be parsed into integer")
+                .expect("cannot be parsed into integer");
+            println!("map: {:?}, res: {}", map, s);
+            s
         })
         .sum()
 }
